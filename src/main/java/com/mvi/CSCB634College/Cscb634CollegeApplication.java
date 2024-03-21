1c9b4289b08bb3f3b6c99e8762fe612fdaf87c34
Simeon <simeonov230@gmail.com>
2024-03-21 23:50:21 +0200

    Maven Project Created with Spring Boot Dependencies

---
+++ src/main/java/com/mvi/CSCB634College/Cscb634CollegeApplication.java
@@ -0,0 +1,13 @@
+package com.mvi.CSCB634College;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cscb634CollegeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cscb634CollegeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mvi/CSCB634College/Cscb634CollegeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvi.CSCB634College;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cscb634CollegeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
