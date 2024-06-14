67386480c4cb787858ae334acd58a853208141b9
Vladislav271828 <vladislavdimovvv@gmail.com>
2024-06-14 13:21:40 +0300

    added check so only professor can change grades

--- src/main/java/com/mvi/CSCB634College/security/config/SecurityConfiguration.java
+++ src/main/java/com/mvi/CSCB634College/security/config/SecurityConfiguration.java
@@ -55,6 +55,7 @@ public class SecurityConfiguration {
                         .requestMatchers("/api/v1/auth/admin/**").hasAuthority("ADMIN")
                         .requestMatchers("/api/v1/user/admin/**").hasAuthority("ADMIN")
                         .requestMatchers("/api/v1/grade/professor/**").hasAuthority("PROFESSOR")
+                        .requestMatchers("/api/v1/absence/professor/**").hasAuthority("PROFESSOR")
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers("/ws-message/**", "/ws/**", "/ws-endpoint/**").permitAll()  // Allow the WebSocket endpoints
                         .requestMatchers("/api/v1/admin/**").hasAuthority("ADMIN")
