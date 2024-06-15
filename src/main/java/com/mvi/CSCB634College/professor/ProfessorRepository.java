86649d56fc23c8ce8ac03e197c9090e2497c1d26
Simeon <simeonov230@gmail.com>
2024-06-15 13:02:58 +0300

    Added get professors by college and department id.

--- src/main/java/com/mvi/CSCB634College/professor/ProfessorRepository.java
+++ src/main/java/com/mvi/CSCB634College/professor/ProfessorRepository.java
@@ -5,10 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 @Transactional
 public interface ProfessorRepository extends JpaRepository<Professor, Integer> {
+    @Query("select p from Professor p where p.department.id = ?1")
+    List<Professor> findByDepartment_Id(Long id);
+    @Query("select p from Professor p where p.department.college.id = ?1")
+    List<Professor> findByDepartment_College_Id(Long id);
     @Query("select p from Professor p where p.user.id = :id")
     Optional<Professor> findProfessorByUserId(@Param("id") Integer id);
 }
---
+++ src/main/java/com/mvi/CSCB634College/professor/ProfessorService.java
@@ -0,0 +1,76 @@
+package com.mvi.CSCB634College.professor;
+
+
+import com.mvi.CSCB634College.department.DtoDepartment;
+import com.mvi.CSCB634College.user.ResponseUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProfessorService {
+
+    private final ProfessorRepository professorRepository;
+    public List<ResponseUser> getAllProfessorsByCollegeId(Long collegeId) {
+
+        List<Professor> professors = professorRepository.findByDepartment_College_Id(collegeId);
+
+        return getResponseUsers(professors);
+    }
+
+
+
+    public List<ResponseUser> getAllProfessorsByDepartmentId(Long departmentId) {
+
+
+        List<Professor> professors = professorRepository.findByDepartment_Id(departmentId);
+
+        return getResponseUsers(professors);
+    }
+
+    private List<ResponseUser> getResponseUsers(List<Professor> professors) {
+        List<ResponseUser> responseUsers = new ArrayList<>();
+        for (Professor professor: professors) {
+            ResponseUser responseUser = new ResponseUser();
+            responseUser.setId(professor.getId());
+            responseUser.setEmail(professor.getUser().getEmail());
+            responseUser.setFirstname(professor.getUser().getFirstname());
+            responseUser.setLastname(professor.getUser().getLastname());
+            responseUser.setRole(professor.getUser().getRole());
+            responseUser.setStudent(null);
+
+            ProfessorDto professorDto = getProfessorDto(professor);
+
+            responseUser.setProfessor(professorDto);
+
+            responseUsers.add(responseUser);
+        }
+
+        return responseUsers;
+    }
+
+    private static ProfessorDto getProfessorDto(Professor professor) {
+        ProfessorDto professorDto = new ProfessorDto();
+        professorDto.setId(professor.getId());
+
+        DtoDepartment dtoDepartment = new DtoDepartment();
+        dtoDepartment.setCollegeId(professor.getDepartment().getCollege().getId());
+        dtoDepartment.setName(professor.getDepartment().getName());
+
+        Integer departmentHadId;
+
+        if (professor.getDepartment().getDepartmentHead() != null){
+            departmentHadId = professor.getDepartment().getDepartmentHead().getId();
+        }else{
+            departmentHadId = null;
+        }
+
+        dtoDepartment.setDepartmentHeadId(departmentHadId);
+
+        professorDto.setDepartment(dtoDepartment);
+        return professorDto;
+    }
+}
