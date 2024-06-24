e479ce72e3cd04d9ea06008956b89733153e9181
Vladislav271828 <vladislavdimovvv@gmail.com>
2024-06-24 11:59:31 +0300

    getNumberOfAbsences now gives 0 as well

--- src/main/java/com/mvi/CSCB634College/absence/AbsenceRepository.java
+++ src/main/java/com/mvi/CSCB634College/absence/AbsenceRepository.java
@@ -17,29 +17,41 @@ public interface AbsenceRepository extends JpaRepository<Absence, Long> {
 
     List<Absence> findAllByEnrollment(Enrollment enrollment);
 
-    @Query("SELECT COUNT(a) FROM Absence a WHERE a.enrollment.student = :student GROUP BY a.enrollment")
-    List<Long> countAbsencesByStudent(
-            @Param("student") Student student);
+    @Query("SELECT COALESCE(COUNT(a), 0) " +
+            "FROM Enrollment e LEFT JOIN e.absences a " +
+            "WHERE e.student = :student " +
+            "GROUP BY e.id")
+    List<Long> countAbsencesByStudent(@Param("student") Student student);
 
-    @Query("SELECT COUNT(a) FROM Absence a WHERE a.enrollment.professor = :professor GROUP BY a.enrollment")
-    List<Long> countAbsencesByProfessor(
-            @Param("professor") Professor professor);
+    @Query("SELECT COALESCE(COUNT(a), 0) " +
+            "FROM Enrollment e LEFT JOIN e.absences a " +
+            "WHERE e.professor = :professor " +
+            "GROUP BY e.id")
+    List<Long> countAbsencesByProfessor(@Param("professor") Professor professor);
 
-    @Query("SELECT COUNT(a) FROM Absence a WHERE FUNCTION('YEAR', a.enrollment.date) = :year GROUP BY a.enrollment")
-    List<Long> countAbsencesByYear(
-            @Param("year") Integer year);
+    @Query("SELECT COALESCE(COUNT(a), 0) " +
+            "FROM Enrollment e LEFT JOIN e.absences a " +
+            "WHERE FUNCTION('YEAR', e.date) = :year " +
+            "GROUP BY e.id")
+    List<Long> countAbsencesByYear(@Param("year") Integer year);
 
-    @Query("SELECT COUNT(a) FROM Absence a WHERE a.enrollment.course = :course GROUP BY a.enrollment")
-    List<Long> countAbsencesByCourse(
-            @Param("course") Course course);
+    @Query("SELECT COALESCE(COUNT(a), 0) " +
+            "FROM Enrollment e LEFT JOIN e.absences a " +
+            "WHERE e.course = :course " +
+            "GROUP BY e.id")
+    List<Long> countAbsencesByCourse(@Param("course") Course course);
 
-    @Query("SELECT COUNT(a) FROM Absence a WHERE a.enrollment.course.major = :major GROUP BY a.enrollment")
-    List<Long> countAbsencesByMajor(
-            @Param("major") Major major);
+    @Query("SELECT COALESCE(COUNT(a), 0) " +
+            "FROM Enrollment e LEFT JOIN e.absences a " +
+            "WHERE e.course.major = :major " +
+            "GROUP BY e.id")
+    List<Long> countAbsencesByMajor(@Param("major") Major major);
 
-    @Query("SELECT COUNT(a) FROM Absence a WHERE a.enrollment.course.major.department.faculty.college = :college GROUP BY a.enrollment")
-    List<Long> countAbsencesByCollege(
-            @Param("college") College college);
+    @Query("SELECT COALESCE(COUNT(a), 0) " +
+            "FROM Enrollment e LEFT JOIN e.absences a " +
+            "WHERE e.course.major.department.faculty.college = :college " +
+            "GROUP BY e.id")
+    List<Long> countAbsencesByCollege(@Param("college") College college);
 
 
 
